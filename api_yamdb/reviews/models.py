cfb14407df8600331077fd331d1d4b3dd39d471c
Ксения Фурсова <kcehna@me.com>
2022-05-01 12:05:32 +0300

    Добавлен мин значение значение рейтинга

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 ROLES_CHOICES = (
@@ -48,7 +48,9 @@ class Review(models.Model):
         related_name='reviews'
     )
     score = models.SmallIntegerField(
-        validators=[MaxValueValidator(10)], default=0)
+        'Оценка',
+        validators=[MaxValueValidator(10), MinValueValidator(1)],
+        default=1)
     pub_date = models.DateTimeField(
         'Дата публикации',
         auto_now_add=True,
@@ -77,7 +79,10 @@ class Comment(models.Model):
     )
     text = models.TextField()
     pub_date = models.DateTimeField(
-        'Дата добавления',
+        'Дата комментария',
         auto_now_add=True,
         db_index=True
     )
+
+    def __str__(self):
+        return self.author
